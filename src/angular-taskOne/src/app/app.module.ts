e8b3813e03025e146e16321ad56f27d8eb79f4e2
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-12 21:01:24 +0600

    Initial dashboard template implemented

--- src/angular-taskOne/src/app/app.module.ts
+++ src/angular-taskOne/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
