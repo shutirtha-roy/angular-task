cc73088ed589d6f55da99dcf63c50fb5cad26d3c
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-13 02:07:56 +0600

    Components added with route for routing through 5 components

--- src/angular-taskOne/src/app/app-routing.module.ts
+++ src/angular-taskOne/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ProductComponent } from './components/product/product.component';
 import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
-  { path: 'user', component: UserComponent}
+  { path: 'user', component: UserComponent},
+  { path: 'modal', component: ModalComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'product', component: ProductComponent},
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
