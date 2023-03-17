35ae20eba5ea843df0b785bb79bf1642de5da121
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-17 13:25:17 +0600

    ng-bootstrap installed

--- src/angular-taskTwo/src/app/app-routing.module.ts
+++ src/angular-taskTwo/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  
+  {
+    path: 'pages',
+    component: PagesComponent
+  }
 ];
 
 @NgModule({
