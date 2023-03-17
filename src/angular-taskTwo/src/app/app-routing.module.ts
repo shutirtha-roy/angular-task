b3fdb381c9a6115b6b441d0fb72390db63d0ea72
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-17 13:25:51 +0600

    Route cleared

--- src/angular-taskTwo/src/app/app-routing.module.ts
+++ src/angular-taskTwo/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [
-  {
-    path: 'pages',
-    component: PagesComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
