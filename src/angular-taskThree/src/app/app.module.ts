c9ef2d6db9b2f9d5e62ad6fa573ade1d5d784935
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-21 20:23:03 +0600

    HTML Template added

--- src/angular-taskThree/src/app/app.module.ts
+++ src/angular-taskThree/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PagesComponent } from './pages/pages.component';
+import { NotfoundComponent } from './shared/components/notfound/notfound.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    NotfoundComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
