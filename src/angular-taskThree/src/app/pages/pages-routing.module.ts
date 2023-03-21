e4c2a1d1fcce371bf3c12a593bd5b6786f856d34
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-22 02:46:47 +0600

    Movie Form Added

--- src/angular-taskThree/src/app/pages/pages-routing.module.ts
+++ src/angular-taskThree/src/app/pages/pages-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieFormComponent } from './movie-form/movie-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movie-form',
+    pathMatch: 'full',
+  }
+  ,
+  {
+    path: 'movie-form',
+    component: MovieFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/angular-taskThree/src/app/pages/pages.module.ts
+++ src/angular-taskThree/src/app/pages/pages.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     
+  
+    MovieFormComponent
   ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
